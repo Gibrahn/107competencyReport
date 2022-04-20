3e3cf142806f91edaf83e09a7cea1bac03cd6f78
Gibrahn Duarte <99296349+Gibrahn@users.noreply.github.com>
2022-04-19 18:59:35 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/navBar";
+import Footer from "./components/footer";
+import Catalog from "./components/catalog";
+import About from "./components/about";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Todo from "./components/todo";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Catalog></Catalog>
+      <About></About>
+      <Todo></Todo>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
